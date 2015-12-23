aa1d2d80da864c3ac1f25130e818853855f5dfa5
Karin Touchie <ktouchie@gmail.com>
2015-12-23 12:38:24 +0100

    Try cocktail sort without limits -- less efficient

--- cocktail.java
+++ cocktail.java
@@ -6,33 +6,26 @@ class cocktail {
         int[] tab = {5,9,4,1,2,7,3,8,6,0};
         
         boolean swap = false;
-        int lastSwapLeft = 0;
-        int lastSwapRight = tab.length-1;
-        int currentSwap = -1;
         
         do {
             swap = false;
-            for (int i= 0; i<lastSwapRight; i++) {
+            for (int i= 0; i<tab.length-1; i++) {
                 if (tab[i]>tab[i+1]) {
                     int x = tab[i];
                     tab[i] = tab[i+1];
                     tab[i+1] = x;
                     swap = true;
-                    currentSwap = i+1;
                 }
             }
-            lastSwapRight = currentSwap;
             
-            for (int j=lastSwapRight-1; j>lastSwapLeft; j--) {
+            for (int j=tab.length-1; j>0; j--) {
                 if (tab[j]<tab[j-1]) {
                     int y = tab[j];
                     tab[j] = tab[j-1];
                     tab[j-1] = y;
                     swap = true;
-                    currentSwap = j-1;
                 }
             }
-            lastSwapLeft = currentSwap;
             
         } while (swap == true);
         System.out.println(Arrays.toString(tab));
