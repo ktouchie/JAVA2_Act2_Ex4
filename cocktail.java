c034f1edcd5a3a5c277c21d23adaa2c4bb6a91e1
Karin Touchie <ktouchie@gmail.com>
2015-12-23 12:24:25 +0100

    Try with for loop (while loops - infinite)

--- cocktail.java
+++ cocktail.java
@@ -6,41 +6,17 @@ class cocktail {
         int[] tab = {5,9,4,1,2,7,3,8,6,0};
         
         boolean swap = false;
-        int j = tab.length-1;
-        int lastSwapRight = -1;
-        int lastSwapLeft = -1;
-        int i = 0;
         
         do {
             swap = false;
-           
-            while (i<j) {
-                
+            for (int i= 0; i<tab.length-1; i++) {
                 if (tab[i]>tab[i+1]) {
                     int x = tab[i];
                     tab[i] = tab[i+1];
                     tab[i+1] = x;
-                    swap = true;                   
-                }
-                lastSwapRight = i;
-            }
-            
-            j = lastSwapRight;
-            
-            while (j>i) {
-                
-                if (tab[j]<tab[j-1]) {
-                    int x = tab[j];
-                    tab[j] = tab[j-1];
-                    tab[j-1] = x;
                     swap = true;
                 }
-                lastSwapLeft = j;
             }
-            
-            i = lastSwapLeft;
-
-            
         } while (swap == true);
         System.out.println(Arrays.toString(tab));
     }
